de39fb3eeb257ae53d278ead084a9c723c2905c5
Natasa <142721571+tatalovicnatasa@users.noreply.github.com>
2025-10-19 22:16:02 +0200

    Clarify load_weights function documentation
    Updated docstring to clarify loading behavior for weights.

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,9 @@ import torch
 def load_weights(model, weights):
     """
     Loads the weights of only the layers present in the given model.
+    Ucitava samo tezine slojeva koji postoje u modelu, ako patch weights ide u whole image model, model ima dodatne PHRefiner blokove a pretrained weights nemaju 
     """
+    # Ucitavanje pretreniranih 
     pretrained_dict = torch.load(weights, map_location='cpu')
     model_dict = model.state_dict()
     pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
